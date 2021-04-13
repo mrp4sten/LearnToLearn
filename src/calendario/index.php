6ed86776e2d76ca9e78e6dfba7e568f8102d1e80
mrp4sten <mauricio.pasten.martinez@gmail.com>
2021-04-13 12:32:40 -0500

    Calendario y Cronometro agregados

---
+++ src/calendario/index.php
@@ -0,0 +1,400 @@
+<?php
+require_once('bdd.php');
+
+
+$sql = "SELECT id, title, start, end, color FROM events ";
+
+$req = $bdd->prepare($sql);
+$req->execute();
+
+$events = $req->fetchAll();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Calendario</title>
+
+	<!-- css -->
+	<link rel="stylesheet" href="../public/css/main.css" />
+
+	<!-- google fonts-->
+	<link rel="preconnect" href="https://fonts.gstatic.com" />
+	<link
+  	href="https://fonts.googleapis.com/css2?family=Raleway&display=swap"
+  	rel="stylesheet"
+	/>
+
+	<!-- font awesome -->
+	<link
+  	rel="stylesheet"
+  	href="https://use.fontawesome.com/releases/v5.15.3/css/all.css"
+  	integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk"
+  	crossorigin="anonymous"
+	/>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+	
+	<!-- FullCalendar -->
+	<link href='css/fullcalendar.css' rel='stylesheet' />
+
+
+    <!-- Custom CSS -->
+
+</head>
+
+<body>
+
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg">
+      <div class="container">
+        <a class="navbar-brand" href="../views/index.html">
+          <i class="fas fa-book"></i>
+          Aprender a aprender
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="fas fa-chevron-circle-down"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="../views/aboutUs.html">Sobre nosotros</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../calendario/index.php">Calendario</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../Clock/index.html">Cronometro</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../todo-list/index.html">Mis Tareas</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="../Alertas/index.html">Consejos</a>
+            </li>
+          </ul>
+
+          <!-- Get Started -->
+          <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item dropdown">
+                <a
+                  class="nav-link dropdown-toggle"
+                  href="#"
+                  id="navbarDarkDropdownMenuLink"
+                  role="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <i class="fas fa-sign-in-alt"></i>
+                  Log in
+                </a>
+                <ul
+                  class="dropdown-menu dropdown-menu-dark"
+                  aria-labelledby="navbarDarkDropdownMenuLink"
+                >
+                  <li>
+                    <a class="dropdown-item" href="./signin.html">Ingresar</a>
+                  </li>
+                  <li>
+                    <a class="dropdown-item" href="./signup.html"
+                      >Crear cuenta</a
+                    >
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+
+          <!-- ond small devices login -->
+          <ul class="navbar-nav">
+            <li class="nav-item d-sm-block d-lg-none">
+              <a class="nav-link btn btn-outline-success" href="./signin.html"
+                >Ingresar</a
+              >
+            </li>
+            <li class="nav-item d-sm-block d-lg-none">
+              <a class="nav-link btn btn-outline-info" href="./signup.html"
+                >Crear cuenta</a
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+                <h1>Calendario De Actividades</h1>
+                <div id="calendar" class="col-md-12">
+            </div>
+			
+        <!-- /.row -->
+		
+		<!-- Modal -->
+		<div class="modal fade" id="ModalAdd" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<form class="form-horizontal" method="POST" action="addEvent.php">
+			
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Agregar Evento</h4>
+			  </div>
+			  <div class="modal-body">
+				
+				  <div class="form-group">
+					<label for="title" class="col-sm-2 control-label">Titulo</label>
+					<div class="col-sm-10">
+					  <input type="text" name="title" class="form-control" id="title" placeholder="Titulo">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="color" class="col-sm-2 control-label">Color</label>
+					<div class="col-sm-10">
+					  <select name="color" class="form-control" id="color">
+									  <option value="">Seleccionar</option>
+						  <option style="color:#0071c5;" value="#0071c5">&#9724; Azul oscuro</option>
+						  <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquesa</option>
+						  <option style="color:#008000;" value="#008000">&#9724; Verde</option>						  
+						  <option style="color:#FFD700;" value="#FFD700">&#9724; Amarillo</option>
+						  <option style="color:#FF8C00;" value="#FF8C00">&#9724; Naranja</option>
+						  <option style="color:#FF0000;" value="#FF0000">&#9724; Rojo</option>
+						  <option style="color:#000;" value="#000">&#9724; Negro</option>
+						  
+						</select>
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="start" class="col-sm-2 control-label">Fecha Inicial</label>
+					<div class="col-sm-10">
+					  <input type="text" name="start" class="form-control" id="start" readonly>
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="end" class="col-sm-2 control-label">Fecha Final</label>
+					<div class="col-sm-10">
+					  <input type="text" name="end" class="form-control" id="end" readonly>
+					</div>
+				  </div>
+				
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-info" data-dismiss="modal">Cerrar</button>
+				<button type="submit" class="btn btn-primary">Guardar</button>
+			  </div>
+			</form>
+			</div>
+		  </div>
+		</div>
+		
+		
+		
+		<!-- Modal -->
+		<div class="modal fade" id="ModalEdit" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+			<div class="modal-content">
+			<form class="form-horizontal" method="POST" action="editEventTitle.php">
+			  <div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">Modificar Evento</h4>
+			  </div>
+			  <div class="modal-body">
+				
+				  <div class="form-group">
+					<label for="title" class="col-sm-2 control-label">Titulo</label>
+					<div class="col-sm-10">
+					  <input type="text" name="title" class="form-control" id="title" placeholder="Titulo">
+					</div>
+				  </div>
+				  <div class="form-group">
+					<label for="color" class="col-sm-2 control-label">Color</label>
+					<div class="col-sm-10">
+					  <select name="color" class="form-control" id="color">
+						  <option value="">Seleccionar</option>
+						  <option style="color:#0071c5;" value="#0071c5">&#9724; Azul oscuro</option>
+						  <option style="color:#40E0D0;" value="#40E0D0">&#9724; Turquesa</option>
+						  <option style="color:#008000;" value="#008000">&#9724; Verde</option>						  
+						  <option style="color:#FFD700;" value="#FFD700">&#9724; Amarillo</option>
+						  <option style="color:#FF8C00;" value="#FF8C00">&#9724; Naranja</option>
+						  <option style="color:#FF0000;" value="#FF0000">&#9724; Rojo</option>
+						  <option style="color:#000;" value="#000">&#9724; Negro</option>
+						  
+						</select>
+					</div>
+				  </div>
+				    <div class="form-group"> 
+						<div class="col-sm-offset-2 col-sm-10">
+						  <div class="checkbox">
+							<label class="text-danger"><input type="checkbox"  name="delete"> Eliminar Evento</label>
+						  </div>
+						</div>
+					</div>
+				  
+				  <input type="hidden" name="id" class="form-control" id="id">
+				
+				
+			  </div>
+			  <div class="modal-footer">
+				<button type="button" class="btn btn-danger" data-dismiss="modal">Cerrar</button>
+				<button type="submit" class="btn btn-primary">Guardar</button>
+			  </div>
+			</form>
+			</div>
+		  </div>
+		</div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery Version 1.11.1 -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+	
+	<!-- FullCalendar -->
+	<script src='js/moment.min.js'></script>
+	<script src='js/fullcalendar/fullcalendar.min.js'></script>
+	<script src='js/fullcalendar/fullcalendar.js'></script>
+	<script src='js/fullcalendar/locale/es.js'></script>
+	
+	
+	<script>
+
+	$(document).ready(function() {
+
+		var date = new Date();
+       var yyyy = date.getFullYear().toString();
+       var mm = (date.getMonth()+1).toString().length == 1 ? "0"+(date.getMonth()+1).toString() : (date.getMonth()+1).toString();
+       var dd  = (date.getDate()).toString().length == 1 ? "0"+(date.getDate()).toString() : (date.getDate()).toString();
+		
+		$('#calendar').fullCalendar({
+			header: {
+				 language: 'es',
+				left: 'prev,next today',
+				center: 'title',
+				right: 'month,basicWeek,basicDay',
+
+			},
+			defaultDate: yyyy+"-"+mm+"-"+dd,
+			editable: true,
+			eventLimit: true, // allow "more" link when too many events
+			selectable: true,
+			selectHelper: true,
+			select: function(start, end) {
+				
+				$('#ModalAdd #start').val(moment(start).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalAdd #end').val(moment(end).format('YYYY-MM-DD HH:mm:ss'));
+				$('#ModalAdd').modal('show');
+			},
+			eventRender: function(event, element) {
+				element.bind('dblclick', function() {
+					$('#ModalEdit #id').val(event.id);
+					$('#ModalEdit #title').val(event.title);
+					$('#ModalEdit #color').val(event.color);
+					$('#ModalEdit').modal('show');
+				});
+			},
+			eventDrop: function(event, delta, revertFunc) { // si changement de position
+
+				edit(event);
+
+			},
+			eventResize: function(event,dayDelta,minuteDelta,revertFunc) { // si changement de longueur
+
+				edit(event);
+
+			},
+			events: [
+			<?php foreach($events as $event): 
+			
+				$start = explode(" ", $event['start']);
+				$end = explode(" ", $event['end']);
+				if($start[1] == '00:00:00'){
+					$start = $start[0];
+				}else{
+					$start = $event['start'];
+				}
+				if($end[1] == '00:00:00'){
+					$end = $end[0];
+				}else{
+					$end = $event['end'];
+				}
+			?>
+				{
+					id: '<?php echo $event['id']; ?>',
+					title: '<?php echo $event['title']; ?>',
+					start: '<?php echo $start; ?>',
+					end: '<?php echo $end; ?>',
+					color: '<?php echo $event['color']; ?>',
+				},
+			<?php endforeach; ?>
+			]
+		});
+		
+		function edit(event){
+			start = event.start.format('YYYY-MM-DD HH:mm:ss');
+			if(event.end){
+				end = event.end.format('YYYY-MM-DD HH:mm:ss');
+			}else{
+				end = start;
+			}
+			
+			id =  event.id;
+			
+			Event = [];
+			Event[0] = id;
+			Event[1] = start;
+			Event[2] = end;
+			
+			$.ajax({
+			 url: 'editEventDate.php',
+			 type: "POST",
+			 data: {Event:Event},
+			 success: function(rep) {
+					if(rep == 'OK'){
+						alert('Evento se ha guardado correctamente');
+					}else{
+						alert('No se pudo guardar. Inténtalo de nuevo.'); 
+					}
+				}
+			});
+		}
+		
+	});
+
+</script>
+
+	<!-- popper and bootstrap.js -->
+	<script
+      src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js"
+      integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js"
+      integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc"
+      crossorigin="anonymous"
+    ></script>
+
+</body>
+
+</html>
