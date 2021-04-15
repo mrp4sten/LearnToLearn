a6b8f45375458c32b33967245c036d7e7cb1402c
LeonR9K <leonfelipe222@gmail.com>
2021-04-14 20:23:40 -0500

    version final

--- login/js/login.js
+++ login/js/login.js
@@ -13,17 +13,14 @@ formulario.addEventListener('submit', e => {
         .then(data => {
             if (data == '0') {
                 console.log('Usuario no encontrado')
-                formLogin.classList.add('inputErroneo');
-                formLogin.children[1].placeholder = 'Usuario no encontrado';
-                formLogin.children[2].placeholder = 'Usuario no encontrado';
+
 
             } else if (data == 'contraseña incorrecta') {
                 console.log(data);
-                formLogin.classList.add('inputErroneo');
-                formLogin.children[2].placeholder = 'Contraseña incorrecta';
+
                 console.log('xd');
             } else {
-                location.href = '../html/inicio.php'
+                location.href = '../index.html';
             }
         })
 })
